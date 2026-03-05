e0b1da2ffacefd549b1bac2a954a8a1b97ff590b
Yoland <yonghelyu03@gmail.com>
2026-03-04 21:39:31 -0800

    Feat: 3 styler changed (3 for each of Appearance & Dimension, 1 for Animation

---
+++ OCKSample/Main/Stylers/AppearanceStyler.swift
@@ -0,0 +1,21 @@
+//
+//  AppearanceStyler.swift
+//  OCKSample
+//
+//  Created by Student on 3/4/26.
+//  Copyright © 2026 Network Reconnaissance Lab. All rights reserved.
+//
+
+import CareKitUI
+import UIKit
+
+struct AppearanceStyler: OCKAppearanceStyler {
+    // Make shadow darker
+    var shadowOpacity1: Float { 0.3 }
+    
+    // Increase corner radius (rounder)
+    var cornerRadius1: CGFloat { 18 }
+
+    // Shift shadow further down
+    var shadowOffset1: CGSize { CGSize(width: 0, height: 4) }
+}
--- OCKSample/Main/Stylers/DimensionStyler.swift
+++ OCKSample/Main/Stylers/DimensionStyler.swift
@@ -1,8 +1,21 @@
 //
-//  Untitled.swift
+//  DimensionStyler.swift
 //  OCKSample
 //
 //  Created by Student on 3/4/26.
 //  Copyright © 2026 Network Reconnaissance Lab. All rights reserved.
 //
 
+import CareKitUI
+import UIKit
+
+struct DimensionStyler: OCKDimensionStyler {
+    // Decrease symbol point size 1 (largest)
+    var symbolPointSize1: CGFloat { 28 }
+
+    // Increase stack spacing
+    var stackSpacing1: CGFloat { 10 }
+
+    // Decrease standard line width
+    var lineWidth1: CGFloat { 2.0 }
+}
