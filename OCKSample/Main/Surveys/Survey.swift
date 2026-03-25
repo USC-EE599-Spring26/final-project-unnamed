76bd63c5e109c5c18f6adc0849b2f49c6dba15a2
Yu-Chieh Huang <jessica.ychuang@gmail.com>
2026-03-24 17:06:02 -0700

    Add initial survey info

---
+++ OCKSample/Main/Surveys/Survey.swift
@@ -0,0 +1,25 @@
+//
+//  Survey.swift
+//  OCKSample
+//
+//  Created by Yu-Chieh on 2026/3/24.
+//  Copyright © 2026 Network Reconnaissance Lab. All rights reserved.
+//
+
+import Foundation
+import CareKitStore
+
+enum Survey: String, CaseIterable, Identifiable {
+    var id: Self { self }
+    case onboard = "Onboard"
+    case rangeOfMotion = "Range of Motion"
+
+    func type() -> Surveyable {
+        switch self {
+        case .onboard:
+            return Onboard()
+        case .rangeOfMotion:
+            return RangeOfMotion()
+        }
+    }
+}
